73e32dbece68f5de4af487d6a83af1c629b62157
russswest <joho141212@outlook.jp>
2018-03-18 11:22:27 +0900

    crudkansei

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  #get 'customers/index'
+
+  #get 'customers/new'
+
+  #get 'customers/create'
+
+  #get 'customers/edit'
+
+  #get 'customers/update'
+
+  #get 'customers/show'
+
+  #get 'customers/destroy'
+
+  resources :customers
+  root "customers#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
